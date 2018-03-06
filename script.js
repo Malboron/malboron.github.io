a5ecdfae5c8edbecefe60f588d9e47a56a0369db
Malboron <33894260+Malboron@users.noreply.github.com>
2018-03-06 19:26:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onscroll = function() {
     var scrolled = window.pageYOffset;
-    parall.style.marginTop=scrolled*0.3+"px";
+    parall.style.marginTop=scrolled*0.001111+"px";
     block.style.marginTop="-"+scrolled+"px";
 }
