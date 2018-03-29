aacbb26b46a258d706199a643af5d7c83290774b
Malboron <alexmalboron@gmail.com>
2018-03-29 13:18:24 +0700

    anim

--- script.js
+++ script.js
@@ -4,13 +4,32 @@ window.onscroll = function() {
     block.style.backgroundPosition="0px "+"-"+scrolled+"px";
 }
 
-var winwid=document.documentElement.clientWidth;
+
 
 window.onload =function(){
     
 }
 
-callb = function(){
+callb.click = function(){
    alert("В разработке..."); 
 }
 
+$(document).ready(function(){
+    $("#b1").click(function(){
+        $("html").animate({
+        scrollTop: "400px"
+        }, 500 );
+    });
+    
+    $("#b2").click(function(){
+        $("html").animate({
+        scrollTop: "800px"
+        }, 500 );
+    });
+    
+    $("#b3").click(function(){
+        $("html").animate({
+        scrollTop: "1200px"
+        }, 500 );
+    });
+});
